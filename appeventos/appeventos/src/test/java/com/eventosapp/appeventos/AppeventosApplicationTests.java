79532efdc51c33738d91ede4c850cf5e8f513f32
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-06-21 12:28:36 -0300

    Clone conexao github

---
+++ appeventos/appeventos/src/test/java/com/eventosapp/appeventos/AppeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventosapp.appeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
